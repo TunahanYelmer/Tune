f79371661f0fd6611d44072d10b779c87d8bceef
Tunahan Yelmer <tunahanyelmer@gmail.com>
2026-08-04 00:22:31 +0300

    feat: update current command to show currently playing track with improved error handling

--- cmd/current.go
+++ cmd/current.go
@@ -1,39 +1,35 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"github.com/tunahanyelmer/Tune/internal/daemon"
 )
 
-// currentCmd represents the current command
 var currentCmd = &cobra.Command{
 	Use:   "current",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Show the currently playing track",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := daemon.EnsureRunning(); err != nil {
+			return err
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("current called")
+		resp, err := daemon.Send(daemon.Request{Action: daemon.ActionCurrent})
+		if err != nil {
+			return err
+		}
+		if !resp.OK {
+			return fmt.Errorf("%s", resp.Error)
+		}
+
+		fmt.Println("Now Playing")
+		fmt.Printf("🎵 %s\n👤 %s\n💿 %s\n", resp.Track.Title, resp.Track.Artist, resp.Track.Album)
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(currentCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// currentCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// currentCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
