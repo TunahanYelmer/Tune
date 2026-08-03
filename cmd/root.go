02ef96ac47a91f0e65ed023dcfa292d6246bbc18
Tunahan Yelmer <tunahanyelmer@gmail.com>
2026-08-04 00:24:40 +0300

    feat: refactor root command structure and update descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -1,49 +1,46 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 
+	"github.com/tunahanyelmer/Tune/internal/config"
+	"github.com/tunahanyelmer/Tune/internal/providers/registry"
 	"github.com/tunahanyelmer/Tune/internal/service"
 )
 
-
-
 var Music *service.MusicService
 
-
-
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "Tune",
-	Short: "Universal terminal music controller",
-	Long:  "Tune is a CLI application for controlling Spotify, YouTube Music, and other music providers.",
-
-
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "tune",
+	Short: "Control your music from the terminal",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() error {
-	return rootCmd.Execute()
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
+
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Tune.git.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cobra.OnInitialize(initMusic)
 }
 
+func initMusic() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "loading config: %v\n", err)
+		os.Exit(1)
+	}
 
+	provider, err := registry.Load(cfg.Provider)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "loading provider: %v\n", err)
+		os.Exit(1)
+	}
+
+	Music = service.NewMusicService(provider)
+}
